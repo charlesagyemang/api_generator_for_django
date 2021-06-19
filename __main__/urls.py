fd78b0c99bec293e7b627d1f809e1535a94f800f
charlesagyemang <micnkru@gmail.com>
2021-06-19 14:50:08 +0000

    ====

--- __main__/urls.py
+++ __main__/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin #type: ignore
 from django.urls import path #type: ignore
-from apps__investments.handlers.investment import InvestmentHandler
+from apps__investments.handlers.investment import InvestmentHandler #type: ignore
 
 # app__users routes Begin ###########
-from apps__users.handlers.role_handler import RoleHandler
-from apps__users.handlers.permission_handler import PermissionHandler
-from apps__users.handlers.userprofile_handler import UserProfileHandler
-from apps__users.handlers.usergroup_handler import UserGroupHandler
-from apps__users.handlers.auth_handler import AuthHandler
+from apps__users.handlers.role_handler import RoleHandler #type: ignore
+from apps__users.handlers.permission_handler import PermissionHandler #type: ignore
+from apps__users.handlers.userprofile_handler import UserProfileHandler #type: ignore
+from apps__users.handlers.usergroup_handler import UserGroupHandler #type: ignore
+from apps__users.handlers.auth_handler import AuthHandler #type: ignore
 # app__users routes End ###########
 
 
--- apps__users/models.py
+++ apps__users/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from utils.constants import TINY_STR_LEN, SHORT_STR_LEN, LONG_STR_LEN
+from django.db import models #type: ignore
+from utils.constants import TINY_STR_LEN, SHORT_STR_LEN, LONG_STR_LEN #type: ignore
 import uuid
 from apps__users import models as user_models
-from django.forms.models import model_to_dict
+from django.forms.models import model_to_dict #type: ignore
 
